4b7bdd5925ff95f1baae34b90214d7c13527709e
kabul Devkota <kabuldevkota@gmail.com>
2024-01-24 14:11:29 +0545

    new project

---
+++ project/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Hearer
+    </div>
+  )
+}
+
+export default Header
---
+++ project/src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import header from './../header/Header';
+import footer from './../footer/Footer';
+import Routers from '../../router/Routers';
+import Header from './../header/Header';
+import Footer from './../footer/Footer';
+
+const Layout = () => {
+  return <>
+   <Header />
+   <Routers />
+   <Footer />
+  
+  </>
+}
+
+export default Layout
